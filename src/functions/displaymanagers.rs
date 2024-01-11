aaed7ad60cb1da4f2de81f5146926081aafc0854
Eshan Roy (Eshanized) <148610067+eshanized@users.noreply.github.com>
2024-01-11 19:21:07 +0530

    update displaymanagers

---
+++ src/functions/displaymanagers.rs
@@ -0,0 +1,32 @@
+use crate::args::DMSetup;
+use crate::args::PackageManager;
+use crate::internal::{files, files_eval, install};
+use crate::internal::services::enable_service;
+
+pub fn install_dm_setup(dm_setup: DMSetup){
+    log::debug!("Installing {:?}", dm_setup);
+    match dm_setup{
+
+    }
+}
+
+fn install_gdm(){
+    install(PackageManager::Pacman, vec![
+        "gdm"
+    ]);
+    enable_service("gdm");
+}
+
+fn install_sddm(){
+    install(PackageManager::Pacman, vec![
+        "sddm"
+    ]);
+    enable_service("sddm");
+}
+
+fn install_lightdm(){
+    install(PackageManager::Pacman, vec![
+        "sddm"
+    ]);
+    enable_service("lightdm")
+}
